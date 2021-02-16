89bb07b82710141bc3cc5c16eed722288912765d
andreibarbarych <anvba@yandex.ru>
2021-02-16 16:09:38 +0300

    updates tests; adds basic test example

--- data-scraper.lib/Model/ScraperDataModel.cs
+++ data-scraper.lib/Model/ScraperDataModel.cs
@@ -2,8 +2,9 @@ namespace DataScraper.Model
 {
 	public class ScraperDataModel
 	{
-		public string Url => @"https://www.livescores.com/";
+		//public string Url => @"https://www.livescores.com/";
 
+		public string Url => @"https://www.livescores.com/soccer/england/league-1/";
 		//@"https://www.livescores.com/soccer/spain/"
 		//@"https://www.livescores.com/soccer/belgium/u21-pro-league-group-1/"
 
--- unit-tests/DataScraperBasicTest.cs
+++ unit-tests/DataScraperBasicTest.cs
@@ -1,41 +1,70 @@
-﻿using Xunit;
-using DataScraper.Model;
-using DataScraper.Scraper;
+﻿using System;
+using Xunit; 
+using DataScraper.Model; 
+using DataScraper.WebResourceConsumer; 
+using DataScraper.Logging;
 
-namespace DataScraper.UnitTests
+namespace DataScraper.UnitTests 
 {
     public class Test_PageScraper
     {
-		[Fact]	
-		public void Test_AtLeastOne_GameScraped()
+		private static GameData NotEmptyGameData = null;
+		
+		public class OnDataItemImpl : IOnDataItem
 		{
-			/*var scraperDataModel = new ScraperDataModel();
-			
-			var driver = scraperDataModel.Url.GetChromeDriver();
-					
-			driver.Title
-
-			var contentDivs = driver.FindElements(By.CssSelector(scraperDataModel.ContentSelector));
-
-			var gameData = new GameData();
-			var pageScraper = new PageScraper(new IScraper[]
-							{
-								new HeaderDataScraper(scraperDataModel.GameConatryAndDate),
-								new GameRowScraper(scraperDataModel.GameData)
-							});
-			
-			foreach(var div in contentDivs)
+			public void OnDataItem(GameData gameData)
 			{
-				pageScraper.ScrapeData(div, gameData);	
+				if (Test_PageScraper.NotEmptyGameData != null)
+						return;
 
-		    	gameData.GameCountry
-		    	gameData.GameLeague
-		    	gameData.GameDate
-				gameData.GameTime
-				gameData.FirstTeam
-				gameData.SecondTeam
-				gameData.GameScore
-			}*/
+				if (!String.IsNullOrWhiteSpace(gameData.GameCountry) &&
+					!String.IsNullOrWhiteSpace(gameData.GameLeague) &&	
+					!String.IsNullOrWhiteSpace(gameData.GameDate) &&
+					!String.IsNullOrWhiteSpace(gameData.GameTime) &&
+					!String.IsNullOrWhiteSpace(gameData.FirstTeam) &&
+					!String.IsNullOrWhiteSpace(gameData.SecondTeam) &&
+					!String.IsNullOrWhiteSpace(gameData.GameScore))
+				{
+					NotEmptyGameData = new GameData
+					{
+						GameCountry = gameData.GameCountry,
+				 		GameLeague = gameData.GameLeague,
+						GameDate = gameData.GameDate,
+						GameTime = gameData.GameTime,
+						FirstTeam = gameData.FirstTeam,
+						SecondTeam = gameData.SecondTeam,
+						GameScore = gameData.GameScore
+					};
+				}	
+			} 
+		}
+
+		[Fact]
+		public void Test_AtLeastOne_GameScraped() 
+		{
+		    var scraperDataModel = new ScraperDataModel(); 
+			var logger = new Logger();
+			var webResource = new WebResource(new OnDataItemImpl(), logger);
+
+			webResource.Initialize(scraperDataModel);
+			webResource.CollectData();
+
+			Assert.NotNull(Test_PageScraper.NotEmptyGameData);
+			Assert.False(Test_PageScraper.NotEmptyGameData.GameCountry.Equals(String.Empty));
+			Assert.False(Test_PageScraper.NotEmptyGameData.FirstTeam.Equals(String.Empty));
+			Assert.False(Test_PageScraper.NotEmptyGameData.GameDate.Equals(String.Empty));
+			Assert.False(Test_PageScraper.NotEmptyGameData.GameLeague.Equals(String.Empty));
+			Assert.False(Test_PageScraper.NotEmptyGameData.GameScore.Equals(String.Empty));
+			Assert.False(Test_PageScraper.NotEmptyGameData.GameTime.Equals(String.Empty));
+			Assert.False(Test_PageScraper.NotEmptyGameData.SecondTeam.Equals(String.Empty));
+
+		    logger.LogInfo("Game Country: " + Test_PageScraper.NotEmptyGameData.GameCountry);	
+		    logger.LogInfo("Game League: " + Test_PageScraper.NotEmptyGameData.GameLeague);	
+		    logger.LogInfo("Game Date: " + Test_PageScraper.NotEmptyGameData.GameDate);	
+			logger.LogInfo("Game Time: " + Test_PageScraper.NotEmptyGameData.GameTime);
+			logger.LogInfo("Game First Team: " + Test_PageScraper.NotEmptyGameData.FirstTeam);
+			logger.LogInfo("Game Second Team: " + Test_PageScraper.NotEmptyGameData.SecondTeam);
+			logger.LogInfo("Game Score: "+ Test_PageScraper.NotEmptyGameData.GameScore);
 		}	
     }
 }
